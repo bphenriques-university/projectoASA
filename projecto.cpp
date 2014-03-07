9829ee1cedd9fb872db11bb1058cf23a801f3a25
Bruno Alexandre Pires Henriques <ist172913@rnlmaster.rnl.ist.utl.pt>
2014-03-07 15:31:16 +0000

    Changed node to class and std::cin is slow. It is now scanf

--- projecto.cpp
+++ projecto.cpp
@@ -3,61 +3,74 @@
 #include <vector>
 #include <stack>
 
+static int index = 0;
 
-typedef struct node {
-	int index;
-	int lowIndex;
-	int value;
-} Node;
+class Node{
+	int _index;
+	int _lowIndex;
+	int _value;
+	
+	public:
+		Node(int value){
+			_index = index++;
+			_lowIndex = -1;
+			_value = value;
+		}
+		
+		int getIndex() { return _index; }
+		int getLowIndex() { return _lowIndex; }
+		int getValue() { return _value; }
+		
+};
 
 std::stack<int> nodeStack;
-std::vector<std::vector<Node> > nodes;
+
+std::vector<Node*> nodes;
 std::vector<std::vector<int> > adjacencias;
-int index = 0;
 
 void tarjanAlgorithm() {
-	for(int i = 0; i < adjacencias.size(); i++) {
-		strongConnect(i);
-	}
+	//for(unsigned int i = 0; i < adjacencias.size(); i++) {
+	//	strongConnect(i);
+	//}
 
 }
 
 
-int main()
-{
-    int N; // number of persons
-    int P; // number of adjacencias
+int main(){
+    int N, P; // number of persons number of shares link
     
     std::cout << "[Number of Persons] [Number of adjacencias]" << std::endl;
     //read N and read P;
-    std::cin >> N >> P;
-    
-    //initialize the share vector equal to number of person
-    nodes = std::vector<std::vector<Node> > (N);
+    scanf("%d %d", &N, &P);
 
-    std::cout << "For each share: [Person a] [Person b]" << std::endl;
+    //initialize the share vector equal to number of person
+    nodes = std::vector<Node*> (N+1);
+    adjacencias = std::vector<std::vector<int> > (N+1);
+	
+	std::cout << "For each share: [Person a] [Person b]" << std::endl;
+    
     //for each share, read
     for(int i = 0 ; i < P ; i++){
 		//person u adjacencias what it receives with person v
 		int u, v;
-		std::cin >> u >> v;
-		Node node;
-		node.index = -1;
-		node.lowIndex = -1;
-		node.value = v;
-
-		nodes[u] = node;
-		adjacencias[u-1].push_back(v);
+		scanf("%d %d", &u, &v);
+		
+		nodes[u] = new Node(v);
+		adjacencias[u].push_back(v);
 	}
 	
 	//for each share, print
-	for(std::vector<std::vector<Node> >::size_type u = 0; u != adjacencias.size(); u++) {
-		for(std::vector<Node>::size_type v = 0; v != adjacencias[u].size(); v++) {
-			std::cout << "Person " << u+1 << " adjacencias what it receives with person " << adjacencias[u][v] << std::endl;
+	for(std::vector<std::vector<int> >::size_type u = 0; u != adjacencias.size(); u++) {
+		for(std::vector<int>::size_type v = 0; v != adjacencias[u].size(); v++) {
+			std::cout << "Person " << u << " adjacencias what it receives with person " << adjacencias[u][v] << std::endl;
 		}
 	}
 	
-	/* rest of the code */
+	for(std::vector<int>::size_type u = 1; u != nodes.size(); u++) {
+			std::cout << "Person " << u << " index: " << nodes[u]->getIndex() << " lowIndex: " << nodes[u]->getLowIndex() << " value: " << nodes[u]->getValue() << std::endl;
+	}
+	
+	
 	tarjanAlgorithm();
     return 0;
 } 
